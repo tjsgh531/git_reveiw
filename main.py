6c1cc0aea06ec9bc21c9b1d4a8990596537431ab
sunho Lee <tjsgh5312@daum.net>
2024-04-19 15:20:04 +0900

    docs : Edit README.md
    Add all information about this project.

---
+++ main.py
@@ -0,0 +1,12 @@
+def check_available():
+    return "It works!"
+
+def split_odd_even(nums=[1, 2, 3, 4]):
+    odds = []
+    evens = []
+    for i in nums:
+        if i % 2 == 0:
+            evens.append(i)
+        else:
+            odds.append(i)
+    return (odds, evens)
